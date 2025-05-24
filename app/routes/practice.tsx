26237a5fab112d90349138416461ba376c0609c5
Tobi Adesanya <adextee001@gmail.com>
2025-05-24 14:57:46 +0100

    .

---
+++ app/routes/practice.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Practice = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-2">
+        <p>Hey</p>
+        <p>Hey</p>
+      </div>
+    </main>
+  );
+};
+
+export default Practice;
