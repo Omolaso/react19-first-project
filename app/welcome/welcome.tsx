5a50a8414dfb2d4b4861b46b242f909c380e7cb9
Tobi Adesanya <adextee001@gmail.com>
2025-05-24 08:32:22 +0100

    updates

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,64 +1,14 @@
 import { useState, type ChangeEvent, type SyntheticEvent } from "react";
 
-// Make one array state of objects, each object will have name and id.
-// On UI iterate over array and show inputs. And also add button to add new object in the array. But before adding new object check if all objects have name and id
-
-interface Props {
-  name: string;
-  id: string;
-}
+// create an array with name and id. Iterate over the array and display the name in an input and id as key.
+// Create a button to add more items into the array and each name input should be modifiable.
+// When adding more items into the array, ensure there isn't an empty field. 
 
 export function Welcome() {
-  const [array, setArray] = useState<Props[]>([
-    { name: "Alice", id: "1" },
-    { name: "John", id: "2" },
-  ]);
-
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
-    const value = e.target.value;
-
-    setArray((prev) =>
-      prev.map((item, arrayIndex) =>
-        arrayIndex === index ? { ...item, name: value } : item
-      )
-    );
-  };
-
-  const handleAddMore = () => {
-    const notEmpty = array.find((item) => item.name === "" || item.id === "");
-
-    if (notEmpty) {
-      alert("empty");
-      return;
-    }
-
-    setArray((prev) => [
-      ...prev,
-      { name: "", id: `${Number(array[array.length - 1].id) + 1}` },
-    ]);
-  };
-
   return (
     <main className="flex items-center justify-center min-h-screen w-full">
       <div className="flex flex-col gap-6">
-        {array.map((item, index) => (
-          <div key={item.id} className="flex flex-row items-center gap-4">
-            <input
-              type="text"
-              className="p-2 border-white border"
-              onChange={(e) => handleOnChange(e, index)}
-              value={array[index].name}
-            />
-          </div>
-        ))}
-
-        <button
-          onClick={handleAddMore}
-          type="button"
-          className="bg-red-400 p-4"
-        >
-          Add More
-        </button>
+        <p>Hi</p>
       </div>
     </main>
   );
